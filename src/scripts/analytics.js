nationalarchives/ds-search

FIND-304: Add tracking for hierarchy pagination
Adds GA4 tracking for the 'Previous' and 'Next' links that
appear within the hierarchy element on details pages.

src/scripts/analytics.js
@@ -68,4 +68,24 @@ if (ga4Id) {
     ],
     "select_hierarchy",
   );
+
+  analytics.addListeners(
+    ".etna-details-hierarchy",
+    "pagination",
+    [
+      {
+        eventName: "tna.select_feature",
+        targetElement: "[rel='next'], [rel='prev']",
+        on: "click",
+        rootData: {
+          data_component_name: "pagination",
+          data_link: ($el) =>
+            $el.getAttribute("rel") === "next" ? "next page" : "previous page",
+          data_link_type: "button",
+          data_section: "Catalogue hierarchy",
+        },
+      },
+    ],
+    "select_feature",
+  );
 }

nationalarchives/ds-catalogue

src/scripts/analytics.js
@@ -69,4 +69,24 @@ if (ga4Id) {
     ],
     "select_hierarchy",
   );
+
+  analytics.addListeners(
+    ".etna-details-hierarchy",
+    "pagination",
+    [
+      {
+        eventName: "tna.select_feature",
+        targetElement: "[rel='next'], [rel='prev']",
+        on: "click",
+        rootData: {
+          data_component_name: "pagination",
+          data_link: ($el) =>
+            $el.getAttribute("rel") === "next" ? "next item" : "previous item",
+          data_link_type: "button",
+          data_section: "Catalogue hierarchy",
+        },
+      },
+    ],
+    "select_feature",
+  );
 }