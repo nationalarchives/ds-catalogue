f47a3c281d13eb18de531aa0d88fba7e48adb284
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-11-04 11:02:44 +0000

    FIND-252:add tests

--- test/search/test_views_long_filter_collection.py
+++ test/search/test_views_long_filter_collection.py
@@ -6,15 +6,14 @@ from django.conf import settings
 from django.test import TestCase
 
 
-class CatalogueSearchViewCollectionFilterMoreOptionsTests(TestCase):
-    """Mainly tests the context.
-    Collection filter is only available for tna group."""
+class CatalogueSearchViewCollectionMoreFilterAttrsTests(TestCase):
+    """Collection filter is only available for tna group."""
 
     @responses.activate
-    def test_search_with_more_options(
+    def test_search_for_more_filter_choices_attributes_without_filters(
         self,
     ):
-        """"""
+        """Tests more filter choices attributes are correctly set in context"""
 
         # data present for input collections
         responses.add(
@@ -75,4 +74,91 @@ class CatalogueSearchViewCollectionFilterMoreOptionsTests(TestCase):
         self.assertEqual(
             collection_field.more_filter_choices_url,
             "?filter_list=longCollection",
+        )
+
+    @responses.activate
+    def test_search_for_more_filter_choices_attributes_with_filters(
+        self,
+    ):
+        """Tests more filter choices attributes are correctly set in context"""
+
+        # data present for input collections
+        responses.add(
+            responses.GET,
+            f"{settings.ROSETTA_API_URL}/search",
+            json={
+                "data": [
+                    {
+                        "@template": {
+                            "details": {
+                                "iaid": "C123456",
+                                "source": "CAT",
+                            }
+                        }
+                    }
+                ],
+                "aggregations": [
+                    {
+                        "name": "collection",
+                        "entries": [
+                            {"value": "BT", "doc_count": 50},
+                            {"value": "WO", "doc_count": 35},
+                        ],
+                        "total": 100,
+                        "other": 50,
+                    }
+                ],
+                "buckets": [
+                    {
+                        "name": "group",
+                        "entries": [
+                            {"value": "tna", "count": 1},
+                        ],
+                    }
+                ],
+                "stats": {
+                    "total": 26008838,
+                    "results": 20,
+                },
+            },
+            status=HTTPStatus.OK,
+        )
+
+        response = self.client.get(
+            "/catalogue/search/?"
+            "q=ufo"
+            "&display=list"
+            "&group=tna"
+            "&online=true"
+            "&covering_date_from-year=1900"
+            "&covering_date_to-year=2024"
+            "&level=Item"
+            "&closure=Open+Document%2C+Open+Description"
+            "&sort=title:asc"
+        )
+
+        context_data = response.context_data
+        form = context_data.get("form")
+        collection_field = form.fields["collection"]
+
+        self.assertEqual(len(context_data.get("results")), 1)
+        self.assertEqual(
+            collection_field.more_filter_choices_available,
+            True,
+        )
+        self.assertEqual(
+            collection_field.more_filter_choices_text, "See more collections"
+        )
+        self.assertEqual(
+            collection_field.more_filter_choices_url,
+            "?q=ufo"
+            "&display=list"
+            "&group=tna"
+            "&online=true"
+            "&covering_date_from-year=1900"
+            "&covering_date_to-year=2024"
+            "&level=Item"
+            "&closure=Open+Document%2C+Open+Description"
+            "&sort=title%3Aasc"
+            "&filter_list=longCollection",
         )
