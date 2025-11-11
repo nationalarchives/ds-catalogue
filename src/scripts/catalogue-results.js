11f10d83989651ae7ca920b263ea936b490bdae8
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-11-11 16:41:19 +0000

    Fix sidebar filters on mobile

--- src/scripts/catalogue-results.js
+++ src/scripts/catalogue-results.js
@@ -10,7 +10,6 @@ if ($searchForm && $sortFormGroup && $sort) {
   });
 }
 
-// search filters for mobile version
 const $mobileFiltersButton = document.getElementById("mobile-filters");
 const $mobileFiltersButtonText = document.getElementById("mobile-filters-text");
 const $visibleAsideElements = document.getElementsByClassName("tna-aside");
@@ -19,74 +18,50 @@ if (
   $mobileFiltersButtonText &&
   $visibleAsideElements.length
 ) {
-  // mobile view
-  const showEventFilters = () => {
-    // console.log('mobile view');
+  const showEventFiltersButton = () => {
     $mobileFiltersButton.style.display = "block";
-    // $visibleAsideElements.style.display = "none";
   };
 
-  // desktop view
-  const hideEventFilters = () => {
-    // console.log('desktop view');
+  const hideEventFiltersButton = () => {
     $mobileFiltersButton.style.display = "none";
   };
+
+  const displayFilters = () => {
+    for (let i = 0; i < $visibleAsideElements.length; i++) {
+      $visibleAsideElements[i].style.display = "block";
+    }
+    $mobileFiltersButtonText.textContent = "Hide filters";
+  };
+
+  const hideFilters = () => {
+    for (let i = 0; i < $visibleAsideElements.length; i++) {
+      $visibleAsideElements[i].style.display = "none";
+    }
+    $mobileFiltersButtonText.textContent = "Show filters";
+  };
+
+  $mobileFiltersButton.addEventListener("click", () => {
+    if ($mobileFiltersButtonText.textContent === "Show filters") {
+      displayFilters();
+    } else {
+      hideFilters();
+    }
+  });
 
   const isMobile = window.matchMedia("(max-width: 48em)");
   isMobile.onchange = (e) => {
     if (e.matches) {
-      showEventFilters();
-    }
-  };
-
-  const isDesktop = window.matchMedia("(min-width: 48em)");
-  isDesktop.onchange = (e) => {
-    if (e.matches) {
-      hideEventFilters();
-    }
-  };
-
-  $mobileFiltersButtonText.textContent = "Show filters";
-
-  $mobileFiltersButton.onclick = function () {
-    if ($mobileFiltersButtonText.textContent === "Show filters") {
-      // If the button says "Add Filters", change it to "Hide Filters" and show the aside elements
-      $mobileFiltersButtonText.textContent = "Hide filters";
-      for (let i = 0; i < $visibleAsideElements.length; i++) {
-        $visibleAsideElements[i].style.display = "block";
-      }
+      showEventFiltersButton();
+      hideFilters();
     } else {
-      // If the button says "Hide Filters", change it to "Add Filters" and hide the aside elements
-      $mobileFiltersButtonText.textContent = "Show filters";
-      for (let i = 0; i < $visibleAsideElements.length; i++) {
-        $visibleAsideElements[i].style.display = "none"; // Changed to 'none' to hide
-      }
+      hideEventFiltersButton();
+      displayFilters();
     }
   };
 
-  // resizing of window
-
-  const displayDesktopFilters = () => {
-    // console.log('show desktop filter');
-    for (let i = 0; i < $visibleAsideElements.length; i++) {
-      $visibleAsideElements[i].style.display = "block";
-    }
-  };
-
-  const displayMobilefilters = () => {
-    // console.log("show mobile filter");
-    for (let i = 0; i < $visibleAsideElements.length; i++) {
-      $visibleAsideElements[i].style.display = "none";
-    }
-  };
-
-  window.addEventListener("resize", () => {
-    // console.log(window.innerWidth);
-    if (window.innerWidth > 400) {
-      displayDesktopFilters();
-    }
-    if (window.innerWidth < 400) {
-      displayMobilefilters();
-    }
-  });
+  if (isMobile.matches) {
+    hideFilters();
+  } else {
+    displayFilters();
+  }
 }
