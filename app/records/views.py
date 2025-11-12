c472c04d3cea3f2c64282759357e465ae296b09a
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-11-12 11:38:50 +0000

    FIND-342:add analytics data context

--- app/records/views.py
+++ app/records/views.py
@@ -40,6 +40,70 @@ class RecordDetailView(
 
     template_name = "records/record_detail.html"
 
+    # TODO: update this implementation with DataLayerMixin once Mixin's
+    # for RecordDetailView is in place.
+    def add_analytics_data_context(self, context) -> None:
+        """Add analytics data to context for data layer tracking."""
+
+        record = context["record"]
+        data = {}
+
+        # Common data for all records
+        if record.is_tna:
+            data["content_group"] = "TNA catalogue"
+        else:
+            data["content_group"] = "Other Archives catalogue"
+
+        class_name = self.__class__.__name__
+        if record.is_tna:
+            if record.is_digitised:
+                data["page_type"] = (
+                    class_name + "[TNA catalogue digitised record description]"
+                )
+            else:
+                data["page_type"] = (
+                    class_name + "[TNA catalogue record description]"
+                )
+        else:
+            data["page_type"] = (
+                class_name + "[Other archive record description]"
+            )
+
+        data["reader_type"] = ""
+
+        # Record-specific data
+        data["user_type"] = ""
+        data["taxonomy_topic"] = ";".join(record.subjects)
+        data["taxonomy_term"] = "not in use"
+        data["time_period"] = "not in use"
+        if record.is_held_by_tna:
+            held_by = "66-The National Archives"
+        else:
+            held_by = record.held_by
+        data["catalogue_repository"] = held_by
+        data["catalogue_level"] = str(record.level_code) + "-" + record.level
+        if series := record.hierarchy_series:
+            data["catalogue_series"] = (
+                series.reference_number + "-" + series.summary_title
+            )
+        else:
+            data["catalogue_series"] = ""
+
+        if record.reference_number and record.summary_title:
+            data["catalogue_reference"] = (
+                record.reference_number + "-" + record.summary_title
+            )
+        else:
+            data["catalogue_reference"] = record.reference_number
+        data["catalogue_datasource"] = record.source
+        data["delivery_option_category"] = context.get(
+            "do_availability_group", ""
+        )
+        data["delivery_option"] = context.get("delivery_option", "")
+
+        # Add the data to the context
+        context["analytics_data"] = data
+
     def get_template_names(self):
         """
         Determine template based on record type.
@@ -61,6 +125,7 @@ class RecordDetailView(
         """Add field labels and all enriched data to context."""
         context = super().get_context_data(**kwargs)
         context["field_labels"] = FIELD_LABELS
+        self.add_analytics_data_context(context=context)
 
         return context
 
