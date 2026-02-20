5649abfc0e60397828bc255dfcec1127d5bfb16d
TNA-Allan <96120886+TNA-Allan@users.noreply.github.com>
2026-02-20 13:46:31 +0000

    FIND-491:revert PR#160 changes as codeql suppress does not work (#161)

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,10 +1,3 @@
-# codeql[unused-global-variable]: Django settings are used dynamically by the framework
-# This intentionally avoids defining __all__ in settings, as it is a CodeQL anti-pattern
-# to suppress unused-variable warnings in this way. Django settings files typically
-# contain many variables that are accessed dynamically.
-# Caveat: genuinely unused variables will not be flagged automatically and must be
-# reviewed manually.
-
 import os
 
 from config.util import strtobool
