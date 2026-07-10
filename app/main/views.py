15fb39cda067f0b421dddc5d0dac180f774ed39d
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2026-07-10 10:58:45 +0100

    FIND-544:refactor Cataloge view to TemplateView to simplify template context handing

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import TemplateView
 
 from app.main.api import (
     fetch_global_notifications,
@@ -19,30 +20,35 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
-def catalogue(request):
+class CatalogueView(TemplateView):
+    template_name = "main/catalogue.html"
 
-    template = loader.get_template("main/catalogue.html")
+    def get_context_data(self, **kwargs):
 
-    explore = get_explore_the_collection()
-    notifications = fetch_global_notifications()
+        context = super().get_context_data(**kwargs)
 
-    # context for the subjects picker
-    subjects_grouped_by_letter = get_subjects_grouped_by_letter()
-    disabled_letters = [
-        letter
-        for letter, subjects in subjects_grouped_by_letter.items()
-        if not subjects
-    ]
+        explore = get_explore_the_collection()
+        notifications = fetch_global_notifications()
 
-    context = {
-        "latest_articles": explore.get("latest_articles", [])[:3],
-        "top_pages": explore.get("top_pages", [])[:3],
-        "global_alert": (notifications.get("global_alert") if notifications else None),
-        "mourning_notice": (
-            notifications.get("mourning_notice") if notifications else None
-        ),
-        "disabled_letters": disabled_letters,
-        "subjects_grouped_by_letter": subjects_grouped_by_letter,
-    }
+        # context for the subjects picker
+        subjects_grouped_by_letter = get_subjects_grouped_by_letter()
+        disabled_letters = [
+            letter
+            for letter, subjects in subjects_grouped_by_letter.items()
+            if not subjects
+        ]
 
-    return HttpResponse(template.render(context, request))
+        context = {
+            "latest_articles": explore.get("latest_articles", [])[:3],
+            "top_pages": explore.get("top_pages", [])[:3],
+            "global_alert": (
+                notifications.get("global_alert") if notifications else None
+            ),
+            "mourning_notice": (
+                notifications.get("mourning_notice") if notifications else None
+            ),
+            "disabled_letters": disabled_letters,
+            "subjects_grouped_by_letter": subjects_grouped_by_letter,
+        }
+
+        return context
