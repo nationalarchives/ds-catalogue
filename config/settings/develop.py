86a34501c00c06dc89475c7619854eb5fa1ee024
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2026-09-09 11:40:33 +0100

    CHORE:fix typo

--- config/settings/develop.py
+++ config/settings/develop.py
@@ -12,7 +12,7 @@ DEBUG = strtobool(os.getenv("DEBUG", "False"))
 
 SENTRY_SAMPLE_RATE = float(os.getenv("SENTRY_SAMPLE_RATE", "1.0"))
 
-# Configure debug toolbar when its installed
+# Configure debug toolbar when it's installed
 if DEBUG and importlib.util.find_spec("debug_toolbar") is not None:
     INSTALLED_APPS += [
         "debug_toolbar",
